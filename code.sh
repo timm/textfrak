7977d25b1323e41c96226832bd1d95a67b51e9ab
Hyperrhiz <hyperrhiz@gmail.com>
2016-04-30 11:41:56 -0400

    saving

--- code.sh
+++ code.sh
@@ -1,8 +1,8 @@
 
 fashion()  { cat data/fashion.txt; }
 downcase() { cat - | tr A-Z a-z ; }
-words()    { cat - | gawk '{for(i=1;i<=NF;i++) if ($i) print $i}' ; }
-tokenize() { cat - | sed 's/[,\.\(\);‘’,{}:;]/ /g'  ; }
+words()    { cat - | awk '{for(i=1;i<=NF;i++) if ($i) print $i}' ; }
+tokenize() { cat - | sed 's/[,\.\(\);‘’,{}:;]/ /g'  ; }
 ngrams()   { cat - | awk '
              BEGIN { split("",a,"") }
                    { a[ length(a) + 1 ] = $1 # here, $1 is the actual word
@@ -12,7 +12,33 @@ ngrams()   { cat - | awk '
 	                   printf("%s ",a[j])
 	                print("")
                    }}'  
-           }	     
-	     
-eg1() { fashion | tokenize | downcase |
-	words | ngrams 3 | sort; } 
+}
+
+lines() { cat - |wc -l; }
+
+has() { cat - | grep $1 ; }
+hasnt() { cat -  | grep -v $1 ; }
+eg1() { fashion  | tokenize | downcase | words  | ngrams 4 | sort; }
+
+eg2() { eg1 | uniq -c | sort -n | awk '$1 > 3'; }
+
+eg3() { eg1 | uniq -c | sort -n  ;  }
+
+eg4() {
+  fashion  | tokenize | downcase | words  | ngrams 4 | sort > /tmp/eg4.out;
+  }
+
+eg5() {
+  eg4
+  grep  more /tmp/eg4.out > /tmp/eg5.more
+  grep less /tmp/eg4.out > /tmp/eg5.less
+}
+
+clean() {
+   rm /tmp/eg*
+}
+
+
+
+
+r() { reload; }
