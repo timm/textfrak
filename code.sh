eb81975fa93896c7be4f04a2c8b9f0390f10d74d
Tim Menzies <tim.menzies@gmail.com>
2016-04-30 08:28:18 -0400

    saving

--- code.sh
+++ code.sh
@@ -5,14 +5,14 @@ words()    { cat - | gawk '{for(i=1;i<=NF;i++) if ($i) print $i}' ; }
 tokenize() { cat - | sed 's/[,\.\(\);‘’,{}:;]/ /g'  ; }
 ngrams()   { cat - | awk '
              BEGIN { split("",a,"") }
-                   { a[ length(a) + 1 ] = $1
-                     m = length(a) - '$1'
+                   { a[ length(a) + 1 ] = $1 # here, $1 is the actual word
+                     m = length(a) - '$1' # here, $1 is the ngram size
                      if (m > 0) {
 	                for(j= m+1 ; j <= length(a); j++)
 	                   printf("%s ",a[j])
 	                print("")
-                   }}'  | sort
+                   }}'  
            }	     
 	     
 eg1() { fashion | tokenize | downcase |
-	words | ngrams 3; } 
+	words | ngrams 3 | sort; } 
