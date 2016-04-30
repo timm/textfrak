1d27a088343f556881385e4f144866ac5fc4a85a
Hyperrhiz <hyperrhiz@gmail.com>
2016-04-30 14:33:38 -0400

    twitter posting

--- src/post2twitter.py
+++ src/post2twitter.py
@@ -1,4 +1,33 @@
+#!/usr/bin/python
+import tweepy
 import sys
+from twitkeys import keys # you need twitkeys.py for your oauth credentials
 
-fred = sys.argv[1]
-print("[",fred,"]")
+
+# Starts the api and auth
+consumer_key = keys['consumer_key']
+consumer_secret = keys['consumer_secret']
+access_token = keys['access_token']
+access_token_secret = keys['access_token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+# Creates the post and logs to a file
+def generate_post():
+
+    output_text = sys.argv[1]
+
+    # Write the status to a file, for logging
+    with open('history.txt', 'a') as f:
+        f.write(output_text + '\n')
+
+    return output_text
+#generate_post()
+
+# Post the status to Twitter
+api.update_status(status=generate_post())
+
---
+++ src/twitkeys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+keys = dict(
+consumer_key = 'xxxxxxxx',
+consumer_secret = 'xxxxxxxx',
+access_token = 'xxxxxxxx',
+access_token_secret = 'xxxxxxxx',
+)
